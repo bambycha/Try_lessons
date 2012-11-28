ce5ad0d981e76d6d404df187844a891e31d9df33
bambycha <2233965@gmail.com>
2012-11-28 17:22:46 +0200

    10

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-# use Rack::Session::Cookie
+use Rack::Session::Cookie
 
-# use OmniAuth::Builder do
-# provider :facebook, '525547224122161', '87080433b183fece8b8fa73fc063c2f7'
-# end
+use OmniAuth::Builder do
+provider :facebook, '525547224122161', '87080433b183fece8b8fa73fc063c2f7'
+end
